ce083965e440321a19dd55f8aa9809ab0cc26065
Chandrahas10 <chandrahaskori9@gmail.com>
2026-01-16 20:33:41 +0530

    add the url of logo

--- Recipe/src/component/Header.jsx
+++ Recipe/src/component/Header.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Body from "./Body";
 import DefaultRecipe from "./DefaultRecipe";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <div className="bg-white shadow-md">
-      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+      <div className="max-w-7xl mx-auto px-6 flex items-center justify-between">
         <div className="flex items-center space-x-3">
           <img
-            src="https://cdn-icons-png.flaticon.com/512/1046/1046784.png"
+            // src="https://cdn-icons-png.flaticon.com/512/1046/1046784.png"
+            src={logo}
             alt="Recipe Logo"
-            className="w-10 h-10"
+            className="w-20 h-20"
           />
-          <h1 className="text-2xl font-bold text-orange-500">RecipeBook</h1>
+          <h1 className="text-2xl font-bold text-orange-500">TastyPages</h1>
         </div>
 
         <nav className="hidden md:flex space-x-8 text-gray-700 font-medium">
